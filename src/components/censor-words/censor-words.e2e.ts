e76b2b87f97700bd869d9877cdb5e98b49eea781
Jordan <iraborjordan1@gmail.com>
2022-07-19 16:13:25 +0100

    includes e2e tests

--- src/components/censor-words/censor-words.e2e.ts
+++ src/components/censor-words/censor-words.e2e.ts
@@ -9,39 +9,16 @@ describe('censor-words', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('renders changes to the name data', async () => {
+  it("check's page configuration", async () => {
     const page = await newE2EPage();
     await page.setContent('<censor-words></censor-words>');
-    const component = await page.find('censor-words');
 
+    const label = await page.find('censor-words >>> label');
+    expect(label.textContent).toEqual(`Enter keywords and phrases to be masked`);
 
-    const element = await page.find('censor-words >>> label');
-    expect(element.textContent).toEqual(`Enter keywords and phrases to be masked`);
+    const button = await page.find('censor-words >>> button');
+    expect(button.textContent).toEqual(`Mask`);
 
-
-    component.setProperty('value', 'donuts')
-    component.setProperty('txtfile', 'pasta')
-    // await page.waitForChanges()
-    // expect(component.censor()).toEqual('XXXX')
-
-
-
-
-
-
-    // component.setProperty('first', 'James');
-
-
-
-
-
-    // await page.waitForChanges();
-    // expect(element.textContent).toEqual(`Hello, World! I'm James`);
-    // component.setProperty('last', 'Quincy');
-    // await page.waitForChanges();
-    // expect(element.textContent).toEqual(`Hello, World! I'm James Quincy`);
-    // component.setProperty('middle', 'Earl');
-    // await page.waitForChanges();
-    // expect(element.textContent).toEqual(`Hello, World! I'm James Earl Quincy`);
+    expect((await button.getComputedStyle()).opacity).toEqual("0.5");
   });
 });
